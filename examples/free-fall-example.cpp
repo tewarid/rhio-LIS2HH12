59ac1c2f71c63e64f51222a86af9f436af54579a
jose1698 <jmarti@rhomb.io>
2020-05-27 10:31:58 +0200

    Upload

--- examples/free-fall-example.cpp
+++ examples/free-fall-example.cpp
@@ -4,6 +4,8 @@
 #include <rhio-pinmap.h>
 
 LIS2HH12 lis = LIS2HH12();
+float x, y, z;
+char x2, y2, z2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,9 +18,11 @@ void setup() {
   lis.setFS(0);          // 2g
   lis.setFDS(ONLPF);
   lis.setPP_OD(PP);
-  lis.setXIE(3, IG1);
-  lis.setYIE(12, IG1);
-  lis.setThresholdIG1(44, 44, 44);
+  lis.setIntMode(128, IG1);
+  lis.setXIE(1, IG1);
+  lis.setYIE(4, IG1);
+  lis.setZIE(16, IG1);
+  lis.setThresholdIG1(90, 90, 90);
   lis.setMinDurationIG(0, IG1);
 }
 
@@ -26,36 +30,20 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (lis.getINT(IG1) == 1) {  // Also you can read only axis INT
     Serial.println("Bro I free-fall");
+    lis.getAccel(&x, &y, &z);
+    Serial.print("x: ");
+    Serial.println(x);
+    Serial.print("y: ");
+    Serial.println(y);
+    Serial.print("z: ");
+    Serial.println(z);
   }
-  float x, y, z;
-  lis.getAccel(&x, &y, &z);
-  Serial.print("x: ");
-  Serial.println(x);
-  Serial.print("y: ");
-  Serial.println(y);
-  Serial.print("z: ");
-  Serial.println(z);
-  char x1, y1, z1, x2, y2, z2;
-  float x11, y11, z11, x22, y22, z22;
-  lis.getAxisHInt(&x1, &y1, &z1, IG1);
   lis.getAxisLInt(&x2, &y2, &z2, IG1);
-  x11 = x1;
-  y11 = y1;
-  z11 = z1;
-  x22 = x2;
-  y22 = y2;
-  z22 = z2;
-  Serial.print("xH: ");
-  Serial.println(x11);
-  Serial.print("yH: ");
-  Serial.println(y11);
-  Serial.print("zH: ");
-  Serial.println(z11);
   Serial.print("xL: ");
-  Serial.println(x22);
+  Serial.println(x2);
   Serial.print("yL: ");
-  Serial.println(y22);
+  Serial.println(y2);
   Serial.print("zL: ");
-  Serial.println(z22);
+  Serial.println(z2);
   delay(2000);
 }
